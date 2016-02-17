2a12c8b9b36b950240a1a2d8c0154982a7e43d59
Ben Thompson <bthomps5@nd.edu>
2016-02-17 15:38:17 -0500

    Passes tests on an example listserv file (to be included in code later)

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# ruby-listserv
+# Listserv
 A Ruby library for parsing Listserv's .list files.
+
+------------
+
+## Purpose
+
+Suppose you have a large number of L-Soft Listserv mailing lists, and need to programmatically read their settings, member lists, and member roles. With this library all you need to do is pass a Listserv .list filename to the Listserv.new() constructor, and you can then access these facets of your mailing list as if it were a typical Ruby object.
+
+## Installation
+
+You can install this library like any other gem:
+
+### From RubyGems
+
+    gem install listserv
+
+### From Bundler
+
+Add this line to your Gemfile
+
+    gem 'listserv'
+
+Then run this command
+
+    bundle install
+    
+
+## Example Usage
+
+    require 'listserv'
+    
+    my_list = Listserv.new('my-list.list')
+    
+    # Print all properties, with the exception of the member list
+    puts my_list.properties
+    
+    # Print an array of listserv members
+    puts my_list.members
--- lib/listserv.rb
+++ lib/listserv.rb
@@ -1,5 +1,6 @@
 class Listserv
 
+  # Iinitialize the class, file name optional
   def initialize(*args)
     if args.size < 1
       @title = ''
@@ -10,6 +11,7 @@ class Listserv
     end
   end
 
+  # Load a file by name
   def load(filename)
     @properties = Hash.new
     offset = 11
@@ -21,16 +23,16 @@ class Listserv
 
     while line = IO.read(file, length, offset += length, mode: 'rb')
       property = line.scan(/\* (\S+)= (.+)/)
-      if property.size == 2
-        key = property[0]
-        if @properties[key] == nil
-          @properties[key] = Array.new
+      hash = property.to_h
+      hash.each do |k, v|
+        if @properties[k] == nil
+          @properties[k] = Array.new
         end
-        property[1].split(',').each do |val|
-          @propertes[key].push val
+        v.strip.split(',').each do |val|
+          @properties[k].push val
         end
       end
-      if key == 'PW'
+      if line.include? ' PW= '
         break
       end
     end
@@ -38,23 +40,48 @@ class Listserv
     @members = Array.new
     while line = IO.read(file, length, offset += length, mode: 'rb')
       member_matches = line.scan(/(.{1,80})[\S]{12}\/\/\/\/    /)
-      if member_matches.count > 0
-        member_matches.flatten.each {|match| @members.push match.strip }
+      if member_matches.flatten!.count > 0
+        member_matches.each do |m|
+          @members.push m.strip
+        end
       end
     end
     return self
   end
 
+  # Get the Listserv's title
   def title
     @title
   end
 
+  # Get a Hash of the Listserv's properties. This includes the members
   def properties
     @properties
   end
 
+  # Get an Array of the Listserv's members. Full names are stripped. Lowercase emails only.
   def members
-    @members
+    get_emails @members
+  end
+
+  # Get the Listserv's password
+  def pw
+    @properties['PW'][0]
+  end
+
+  # Get the Listserv's owners in an array of email addresses
+  def owners
+    get_emails @properties['Owner']
+  end
+
+  # Get the Listserv's editors in an array of email addresses
+  def editors
+    get_emails @properties['Editor']
+  end
+
+  # Get the Listserv's moderators in an array of email addresses
+  def moderators
+    get_emails @properties['Moderator']
   end
 
 
